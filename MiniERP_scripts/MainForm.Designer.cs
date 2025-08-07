99d78bcef6ad92931e629f74bb3b21b7bcdb383c
Trhlito <cznorad@gmail.com>
2025-08-07 20:20:43 +0200

    Update MainForm.Designer.cs

--- MiniERP_scripts/MainForm.Designer.cs
+++ MiniERP_scripts/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ERP3
+namespace ERP3
 {
     partial class MainForm
     {
@@ -31,11 +31,12 @@
             this.btnCustomers = new System.Windows.Forms.Button();
             this.btnHomepage = new System.Windows.Forms.Button();
             this.panelContent = new System.Windows.Forms.Panel();
+
             this.panelMenu.SuspendLayout();
             this.SuspendLayout();
+
             // 
             // panelMenu
-            // 
             this.panelMenu.BackColor = System.Drawing.Color.LightSteelBlue;
             this.panelMenu.Controls.Add(this.btnExit);
             this.panelMenu.Controls.Add(this.lblTitle);
@@ -49,19 +50,19 @@
             this.panelMenu.Name = "panelMenu";
             this.panelMenu.Size = new System.Drawing.Size(150, 450);
             this.panelMenu.TabIndex = 0;
+
             // 
             // btnExit
-            // 
             this.btnExit.Location = new System.Drawing.Point(12, 340);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(60, 30);
             this.btnExit.TabIndex = 5;
-            this.btnExit.Text = "Exit";
+            this.btnExit.Text = "Konec";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+
             // 
             // lblTitle
-            // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Bold);
             this.lblTitle.ForeColor = System.Drawing.SystemColors.GrayText;
@@ -71,64 +72,64 @@
             this.lblTitle.TabIndex = 6;
             this.lblTitle.Text = "MiniERP";
             this.lblTitle.Click += new System.EventHandler(this.label1_Click);
+
             // 
             // btnStock
-            // 
             this.btnStock.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnStock.Location = new System.Drawing.Point(12, 285);
             this.btnStock.Name = "btnStock";
             this.btnStock.Size = new System.Drawing.Size(130, 40);
             this.btnStock.TabIndex = 4;
-            this.btnStock.Text = "Stock";
+            this.btnStock.Text = "Sklad";
             this.btnStock.UseVisualStyleBackColor = true;
             this.btnStock.Click += new System.EventHandler(this.btnStock_Click);
+
             // 
             // btnOrders
-            // 
             this.btnOrders.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnOrders.Location = new System.Drawing.Point(12, 235);
             this.btnOrders.Name = "btnOrders";
             this.btnOrders.Size = new System.Drawing.Size(130, 40);
             this.btnOrders.TabIndex = 3;
-            this.btnOrders.Text = "Orders";
+            this.btnOrders.Text = "Objednávky";
             this.btnOrders.UseVisualStyleBackColor = true;
             this.btnOrders.Click += new System.EventHandler(this.btnOrders_Click);
+
             // 
             // btnProducts
-            // 
             this.btnProducts.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnProducts.Location = new System.Drawing.Point(12, 185);
             this.btnProducts.Name = "btnProducts";
             this.btnProducts.Size = new System.Drawing.Size(130, 40);
             this.btnProducts.TabIndex = 2;
-            this.btnProducts.Text = "Products";
+            this.btnProducts.Text = "Produkty";
             this.btnProducts.UseVisualStyleBackColor = true;
             this.btnProducts.Click += new System.EventHandler(this.btnProducts_Click);
+
             // 
             // btnCustomers
-            // 
             this.btnCustomers.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnCustomers.Location = new System.Drawing.Point(12, 135);
             this.btnCustomers.Name = "btnCustomers";
             this.btnCustomers.Size = new System.Drawing.Size(130, 40);
             this.btnCustomers.TabIndex = 1;
-            this.btnCustomers.Text = "Customers";
+            this.btnCustomers.Text = "Zákazníci";
             this.btnCustomers.UseVisualStyleBackColor = true;
             this.btnCustomers.Click += new System.EventHandler(this.btnCustomers_Click);
+
             // 
             // btnHomepage
-            // 
             this.btnHomepage.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnHomepage.Location = new System.Drawing.Point(12, 85);
             this.btnHomepage.Name = "btnHomepage";
             this.btnHomepage.Size = new System.Drawing.Size(130, 40);
             this.btnHomepage.TabIndex = 0;
-            this.btnHomepage.Text = "Homepage";
+            this.btnHomepage.Text = "Úvodní stránka";
             this.btnHomepage.UseVisualStyleBackColor = true;
             this.btnHomepage.Click += new System.EventHandler(this.btnHomepage_Click);
+
             // 
             // panelContent
-            // 
             this.panelContent.BackColor = System.Drawing.Color.WhiteSmoke;
             this.panelContent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -136,14 +137,14 @@
             this.panelContent.Name = "panelContent";
             this.panelContent.Size = new System.Drawing.Size(650, 450);
             this.panelContent.TabIndex = 1;
+
             // 
             // MainForm
-            // 
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panelContent);
             this.Controls.Add(this.panelMenu);
             this.Name = "MainForm";
-            this.Text = "MiniERP - Main Window";
+            this.Text = "MiniERP - Hlavní okno";
             this.panelMenu.ResumeLayout(false);
             this.panelMenu.PerformLayout();
             this.ResumeLayout(false);
