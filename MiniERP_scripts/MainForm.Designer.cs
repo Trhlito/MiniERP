d8213e8e00376eec0ece9f7b0f8e9d9b7b067787
Trhlito <cznorad@gmail.com>
2025-08-08 09:05:52 +0200

    Update MainForm.Designer.cs

--- MiniERP_scripts/MainForm.Designer.cs
+++ MiniERP_scripts/MainForm.Designer.cs
@@ -20,6 +20,8 @@ namespace ERP3
             base.Dispose(disposing);
         }
 
+        
+        //Vlastnosti 
         private void InitializeComponent()
         {
             this.panelMenu = new System.Windows.Forms.Panel();
@@ -35,8 +37,7 @@ namespace ERP3
             this.panelMenu.SuspendLayout();
             this.SuspendLayout();
 
-            // 
-            // panelMenu
+            // Hlavní Menu
             this.panelMenu.BackColor = System.Drawing.Color.LightSteelBlue;
             this.panelMenu.Controls.Add(this.btnExit);
             this.panelMenu.Controls.Add(this.lblTitle);
@@ -51,8 +52,7 @@ namespace ERP3
             this.panelMenu.Size = new System.Drawing.Size(150, 450);
             this.panelMenu.TabIndex = 0;
 
-            // 
-            // btnExit
+            // Tlačítko pro Ukončení aplikace
             this.btnExit.Location = new System.Drawing.Point(12, 340);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(60, 30);
@@ -61,8 +61,8 @@ namespace ERP3
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
 
-            // 
-            // lblTitle
+
+            // Titul
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Bold);
             this.lblTitle.ForeColor = System.Drawing.SystemColors.GrayText;
@@ -73,8 +73,7 @@ namespace ERP3
             this.lblTitle.Text = "MiniERP";
             this.lblTitle.Click += new System.EventHandler(this.label1_Click);
 
-            // 
-            // btnStock
+            // Tlačítko: Sklad
             this.btnStock.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnStock.Location = new System.Drawing.Point(12, 285);
             this.btnStock.Name = "btnStock";
@@ -84,8 +83,7 @@ namespace ERP3
             this.btnStock.UseVisualStyleBackColor = true;
             this.btnStock.Click += new System.EventHandler(this.btnStock_Click);
 
-            // 
-            // btnOrders
+            // Tlačítko: Objednávky
             this.btnOrders.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnOrders.Location = new System.Drawing.Point(12, 235);
             this.btnOrders.Name = "btnOrders";
@@ -95,8 +93,7 @@ namespace ERP3
             this.btnOrders.UseVisualStyleBackColor = true;
             this.btnOrders.Click += new System.EventHandler(this.btnOrders_Click);
 
-            // 
-            // btnProducts
+            // Tlačítko: Produkty
             this.btnProducts.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnProducts.Location = new System.Drawing.Point(12, 185);
             this.btnProducts.Name = "btnProducts";
@@ -106,8 +103,8 @@ namespace ERP3
             this.btnProducts.UseVisualStyleBackColor = true;
             this.btnProducts.Click += new System.EventHandler(this.btnProducts_Click);
 
-            // 
-            // btnCustomers
+
+            // Tlačítko: Zákazníci
             this.btnCustomers.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnCustomers.Location = new System.Drawing.Point(12, 135);
             this.btnCustomers.Name = "btnCustomers";
@@ -117,8 +114,7 @@ namespace ERP3
             this.btnCustomers.UseVisualStyleBackColor = true;
             this.btnCustomers.Click += new System.EventHandler(this.btnCustomers_Click);
 
-            // 
-            // btnHomepage
+            // Domovská stránka
             this.btnHomepage.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnHomepage.Location = new System.Drawing.Point(12, 85);
             this.btnHomepage.Name = "btnHomepage";
@@ -128,8 +124,7 @@ namespace ERP3
             this.btnHomepage.UseVisualStyleBackColor = true;
             this.btnHomepage.Click += new System.EventHandler(this.btnHomepage_Click);
 
-            // 
-            // panelContent
+            // Obsah
             this.panelContent.BackColor = System.Drawing.Color.WhiteSmoke;
             this.panelContent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -138,8 +133,7 @@ namespace ERP3
             this.panelContent.Size = new System.Drawing.Size(650, 450);
             this.panelContent.TabIndex = 1;
 
-            // 
-            // MainForm
+            // Vlastnosti Okna
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panelContent);
             this.Controls.Add(this.panelMenu);
